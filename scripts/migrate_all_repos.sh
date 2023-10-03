c101f1c0676b1a2d85846ac1fb8c789d118d578f
Ben Artuso <benr2so@gmail.com>
2023-10-03 14:47:25 -0400

    update scripts to read from list of valid repos

--- scripts/migrate_all_repos.sh
+++ scripts/migrate_all_repos.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 ROOT_PATH=$1
-MIGRATION_NAME=$2
+VALID_REPOS_FILE=$2
+MIGRATION_NAME=$3
 TIMESTAMP=$(date "+%Y%m%d-%H%M%S")
 BUCKET_NAME="test-repo-backups"
 
 
-if [ -z "$ROOT_PATH" ] || [ -z "$MIGRATION_NAME" ]; then
-  echo "Usage: $0 <root_path> <migration_name>"
+if [ -z "$ROOT_PATH" ] || [ -z "$MIGRATION_NAME" ] || [ -z "$VALID_REPOS_FILE" ]; then
+  echo "Usage: $0 <root_path> <valid_repos_file> <migration_name>"
   exit 1
 fi
 
@@ -20,6 +21,11 @@ else
     ABSOLUTE_ROOT_PATH="$(realpath $ROOT_PATH)"
 fi
 
+if [ ! -e "$VALID_REPOS_FILE" ]; then
+    echo "$VALID_REPOS_FILE does not exist. Exiting."
+    exit 1
+fi
+
 # Get latest run in s3
 latest_datetime=$(aws s3 ls s3://$BUCKET_NAME/ | sort | awk '{print $NF}' | tail -n 1)
 latest_backup_path="s3://$BUCKET_NAME/$(echo $latest_datetime | sed 's/\/$//')"
@@ -29,48 +35,39 @@ echo "Found latest s3 backup: $latest_datetime"
 echo "Verifying all namespaces are backed up before migration..."
 
 # First check that all namespaces we're about to migrate exist in the latest s3 backup.
-for namespace in "$ABSOLUTE_ROOT_PATH"/*; do
 
-  if [ -d "$namespace" ]; then
-    namespace_name=$(basename "$namespace")
+while IFS= read -r line; do 
+    namespace_name="${line%%/*}"  # Extracting namespace_name
+    repository_name="${line##*/}"   
 
-    for repository in "$namespace"/*; do
-      if [ -d "$repository" ]; then
-        repository_name=$(basename "$repository")
+    ABSOLUTE_REPO_PATH="$ABSOLUTE_ROOT_PATH/$namespace_name/$repository_name"
 
-          # Make sure this repository is included in the most recent backup, otherwise exit 
-          echo "Checking for path... $latest_backup_path/$namespace_name/$repository_name.tar.gz"
-          aws s3 ls "$latest_backup_path/$namespace_name/$repository_name.tar.gz"
-          if [ $? -ne 0 ]; then
-            echo "ERROR: $repository_name missing from latest S3 backup, exiting."
-            exit 1
-          fi
-      fi  
-    done  
-  fi  
-done  
+    if [ ! -d "$ABSOLUTE_REPO_PATH" ]; then
+        echo "ERROR: $ABSOLUTE_REPO_PATH does not exist, exiting."
+        exit 1
+    fi
+
+    aws s3 ls "$latest_backup_path/$namespace_name/$repository_name.tar.gz"
+      if [ $? -ne 0 ]; then
+        echo "ERROR: $repository_name missing from latest S3 backup, exiting."
+        exit 1
+    fi
+done < "$VALID_REPOS_FILE"
 
 echo "Verification complete. Migrating namespaces..."
+while IFS= read -r line; do 
+    namespace_name="${line%%/*}"  # Extracting namespace_name
+    repository_name="${line##*/}"   
 
-for namespace in "$ABSOLUTE_ROOT_PATH"/*; do
+    ABSOLUTE_REPO_PATH="$ABSOLUTE_ROOT_PATH/$namespace_name/$repository_name"
+    # Run the migration 
+    oxen migrate up "$MIGRATION_NAME" "$namespace_name/$repository_name" 
 
-  if [ -d "$namespace" ]; then
-    namespace_name=$(basename "$namespace")
-
-    for repository in "$namespace"/*; do
-      if [ -d "$repository" ]; then
-        repository_name=$(basename "$repository")
-          # Run the migration 
-          oxen migrate up "$MIGRATION_NAME" "$namespace_name/$repository_name" 
-
-          if [ $? -ne 0 ]; then
-            echo "Migration failed, exiting."
-            exit 1
-        fi  
-      fi  
-    done  
-  fi  
-done  
+    if [ $? -ne 0 ]; then
+      echo "Migration failed, exiting."
+      exit 1
+    fi  
+done < "$VALID_REPOS_FILE"
 
 echo "All migrations complete."   
 
